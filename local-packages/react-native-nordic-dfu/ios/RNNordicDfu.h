a8649083acb7bb9338100c5fb2f2d2314d1d4db2
Victor <55268669+victor-wildlife@users.noreply.github.com>
2025-12-22 22:18:06 +1300

    Fix: Use correct module name iOSDFULibrary in header import

--- local-packages/react-native-nordic-dfu/ios/RNNordicDfu.h
+++ local-packages/react-native-nordic-dfu/ios/RNNordicDfu.h
@@ -1,13 +1,14 @@
 #import <CoreBluetooth/CoreBluetooth.h>
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
-#import <NordicDFU/NordicDFU-Swift.h>
+#import <iOSDFULibrary/iOSDFULibrary-Swift.h>
 
-@interface RNNordicDfu : RCTEventEmitter<RCTBridgeModule, DFUServiceDelegate, DFUProgressDelegate, LoggerDelegate>
+@interface RNNordicDfu : RCTEventEmitter <RCTBridgeModule, DFUServiceDelegate,
+                                          DFUProgressDelegate, LoggerDelegate>
 
-@property (strong, nonatomic) NSString * deviceAddress;
-@property (strong, nonatomic) RCTPromiseResolveBlock resolve;
-@property (strong, nonatomic) RCTPromiseRejectBlock reject;
+@property(strong, nonatomic) NSString *deviceAddress;
+@property(strong, nonatomic) RCTPromiseResolveBlock resolve;
+@property(strong, nonatomic) RCTPromiseRejectBlock reject;
 
 + (void)setCentralManagerGetter:(CBCentralManager * (^)())getter;
 + (void)setOnDFUComplete:(void (^)())onComplete;
