3d2670266e303c0049fd9dfc9bd067f3708bd81c
Victor <55268669+victor-wildlife@users.noreply.github.com>
2026-07-25 09:33:28 +1200

    chore: bump version to 0.0.61 and check is_active in add_project_member

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -12,7 +12,7 @@
 import { appSchema, tableSchema } from '@nozbe/watermelondb'
 
 export default appSchema({
-    version: 400,
+    version: 402,
     tables: [
         tableSchema({
             name: 'account_deletion_requests',
